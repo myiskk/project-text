1d15f31da103945accf9d1af93da7b583dd6a476
luwenliang <luwenliang@sohu-inc.com>
2018-02-08 18:43:20 +0800

    node

--- node-test/requestHandlers.js
+++ node-test/requestHandlers.js
@@ -1,19 +1,22 @@
-function start() {
-    console.log("Request handler 'start' was called.");
-  
-    function sleep(milliSeconds) {
-      var startTime = new Date().getTime();
-      while (new Date().getTime() < startTime + milliSeconds);
-    }
-  
-    sleep(10000);
-    return "Hello Start";
-  }
-  
-  function upload() {
-    console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
-  }
-  
-  exports.start = start;
-  exports.upload = upload;
+var exec = require("child_process").exec;
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    // response.write(1111111111111, stdout);
+    response.write("dddddd");
+    response.end();
+  });
+}
+
+function upload(response) {
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
+}
+
+exports.start = start;
+exports.upload = upload;
