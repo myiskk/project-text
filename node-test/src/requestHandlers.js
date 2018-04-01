8d05276a5a3e5d92ff06d5705733155c2fc19fb5
梁露文 <liangluwen@liangluwendeMBP.lan>
2018-04-01 16:33:42 +0800

    添加忽略

--- node-test/src/requestHandlers.js
+++ node-test/src/requestHandlers.js
@@ -1,16 +1,47 @@
+var mysql      = require('mysql');
 var querystring = require("querystring"),
     fs = require("fs"),
     formidable = require("formidable");
 
+var connection = mysql.createConnection({
+  host     : 'liangdb.c7fn0orhmfju.ap-northeast-2.rds.amazonaws.com',
+  user     : 'root',
+  password : '123456llw',
+  database : 'text_db'
+});
+  
+connection.connect();
+
+const header1 = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "x-requested-with,content-type"
+}
 function start(response) {
   console.log("Request handler 'start' was called.");
-    var params = {
-      'a': 123,
-      'b': 456
-    }
-    response.writeHead(200, {"Content-Type": "text/html"});
-    // response.write('ssssss');
-    response.end(JSON.stringify(params));
+
+  connection.query('SELECT * FROM user', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify(results));
+  });
+}
+
+function sqltext (response) {
+  // var params = {
+  //   'a': 111,
+  //   'b': 222
+  // }
+  // response.writeHead(200, {"Content-Type": "application/json"});
+  // response.end(JSON.stringify(params));
+
+  // connection.query('SELECT * FROM user', function (error, results, fields) {
+  //   if (error) throw error;
+  //   console.log('The solution is: ', results);
+  //   response.writeHead(200, {"Content-Type": "application/json"});
+  //   response.end(JSON.stringify(results));
+  // });
 }
 
 function upload(response, request) {
@@ -46,3 +77,4 @@ function show(response) {
 exports.start = start;
 exports.upload = upload;
 exports.show = show;
+exports.sqltext = sqltext;
