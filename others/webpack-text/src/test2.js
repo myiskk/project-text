d1b723a84497af527884836524911834383c9f63
luwenliang <luwenliang@sohu-inc.com>
2018-06-04 09:58:19 +0800

    玩的

---
+++ others/webpack-text/src/test2.js
@@ -0,0 +1,6 @@
+
+function T2 () {
+    console.log('i am t2');
+}
+
+module.export = T2;
---
+++ others/webpack-text/webpack.config.js
@@ -0,0 +1,8 @@
+var webpack = require('webpack');
+module.exports = {
+    mode: 'development',
+    entry: './src/test1.js',
+    output: {
+        filename: 't1.js'
+    }
+}
