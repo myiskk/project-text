2f57b5a5c2a0aed47d459b86cf6c74322727fe70
luwenliang <luwenliang@sohu-inc.com>
2018-11-19 20:27:01 +0800

    dom尝试

--- to-share/3-lightapp/src/request.js
+++ to-share/3-lightapp/src/request.js
@@ -3,7 +3,8 @@
 
 function getFeedListData (successFn) {
     fetch.fetch({
-        url: 'https://v2.sohu.com/integration-api/mix/region/16?page=1&size=40&pvId=1541144404097f4sQFqJ&mpId=272774864&requestId=1811021327472OMV_1541144404196',
+        url: 'http://v2.sohu.com/integration-api/pure/feedByRegion/16?page=1&size=40',
+        method: 'GET',
         success: function (response) {
           successFn(response);
         },
